4548f6e1db1df77c1d41a39d3cdd921a636fd4c6
Jesse Sher <jebba272727@gmail.com>
2026-02-14 15:18:16 +1100

    added code

--- script.py
+++ script.py
@@ -1,7 +1,15 @@
-import openai, os
+import openai, os, random, math
 
 while True:
     user = input('Say "Go" to commit!: ')
 
     if user.lower() == "go":
-        os.system("ls")
+        n = random.randint(0, 10000000000000000)
+        print(n)
+
+        with open("file.txt", "w") as f:
+            f.write(str(n))
+
+        os.system("git add .")
+        os.system(f'git commit -m "My Lucky Number is {n}!"')
+        os.system("git push")
